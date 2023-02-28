b8b7ab2ccaf87e02237ec5b7534895b9559a49be
ParkJeongHo <pjh31208@naver.com>
2023-02-28 20:56:50 +0900

    Feat: Create root components for  rental's components

--- src/components/Rental/Type.tsx
+++ src/components/Rental/Type.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import RentalInput from './Input';
+import { useDispatch } from 'react-redux';
+import { setRentalType } from '../../redux/reducer/inputSlice';
+import { Button, Title, TypeBox } from '../../styles/Rental.style';
+import useCustomSelector from '../../hooks/useCustomSelector';
 
 const RentalType = () => {
-  return <div>RentalType</div>;
+  const dispatch = useDispatch();
+  const { RentalType } = useCustomSelector().inputState;
+  const [isClickedMontly, setIsClickedMontly] = useState(false);
+  const [isClickedCharter, setIsClickedCharter] = useState(false);
+
+  const handleClickMontly = () => {
+    setIsClickedMontly(!isClickedMontly);
+    setIsClickedCharter(false);
+    dispatch(setRentalType('월세'));
+  };
+
+  const handleClickCharter = () => {
+    setIsClickedCharter(!isClickedCharter);
+    setIsClickedMontly(false);
+    dispatch(setRentalType('전세'));
+  };
+
+  useEffect(() => {
+    setIsClickedMontly(RentalType === '월세');
+    setIsClickedCharter(RentalType === '전세');
+  }, [RentalType]);
+
+  return (
+    <>
+      <Title>임대유형</Title>
+      <TypeBox>
+        <Button isClicked={isClickedMontly} onClick={handleClickMontly}>
+          월세
+        </Button>
+        <Button isClicked={isClickedCharter} onClick={handleClickCharter}>
+          전세
+        </Button>
+      </TypeBox>
+      <RentalInput isClickedMontly={isClickedMontly} isClickedCharter={isClickedCharter} />
+    </>
+  );
 };
 
 export default RentalType;
--- src/components/Rental/index.tsx
+++ src/components/Rental/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import RentalHeader from './Header';
 import RentalType from './Type';
+import { RentalWrapper } from '../../styles/Rental.style';
 
 const Rental = () => {
   return (
-    <>
+    <RentalWrapper>
       <RentalHeader />
       <RentalType />
-    </>
+    </RentalWrapper>
   );
 };
 
