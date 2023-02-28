cff9893fb71f20199ba403d27edb79ec326b2fae
ParkJeongHo <pjh31208@naver.com>
2023-02-28 20:56:12 +0900

    Feat(header): Create header in rentalpage

--- src/components/Rental/Header.tsx
+++ src/components/Rental/Header.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { HeaderWrapper } from '../../styles/Rental.style';
 
 const RentalHeader = () => {
-  return <div>RentalHeader</div>;
+  return (
+    <HeaderWrapper>
+      전월세 비용을 입력하시면 <br />내 월세 환급금을 알려드려요 👇
+    </HeaderWrapper>
+  );
 };
 
 export default RentalHeader;
