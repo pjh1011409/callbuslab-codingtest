5fd69b4580e04e67d1f1a526abd998b39d6e0f58
ParkJeongHo <pjh31208@naver.com>
2023-02-28 21:11:35 +0900

    Feat: Create root components for residencypage's components

--- src/components/Residency/index.tsx
+++ src/components/Residency/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { ResidencyWrapper } from '../../styles/Residency.style';
 import ResidencyHeader from './Header';
 import ResidencyInput from './Input';
 
 const Residency = () => {
   return (
-    <>
+    <ResidencyWrapper>
       <ResidencyHeader />
       <ResidencyInput />
-    </>
+    </ResidencyWrapper>
   );
 };
 
