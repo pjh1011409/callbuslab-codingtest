3dee15131bda6bf072e7216f492e81d2c8d2575f
ParkJeongHo <pjh31208@naver.com>
2023-02-28 20:52:55 +0900

    Feat(types): Create type definition

--- src/hooks/hook.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const hook = () => {
-  return <div>hook</div>;
-};
-
-export default hook;
--- src/types/type.ts
+++ src/types/type.ts
@@ -1,1 +1,18 @@
-// type
+export interface InputState {
+  RentalType: string;
+  Deposit: string;
+  MontlyRent: string;
+  MontlyFee: string;
+  PaymentDate: string;
+  Address: string;
+  RoomNumber: string;
+  TenantName: string;
+  StartDate: string;
+  EndDate: string;
+  Phone: string;
+}
+
+export interface RentalInputProps {
+  isClickedCharter: boolean;
+  isClickedMontly: boolean;
+}
