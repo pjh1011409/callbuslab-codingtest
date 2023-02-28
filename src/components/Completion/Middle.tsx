1e72ce90e407bf741591becd6e14ff19f900cb90
ParkJeongHo <pjh31208@naver.com>
2023-02-28 21:21:06 +0900

    Feat: Create middle data in completionBox
    1. 임대유형, 월임대료, 보증금, 월관리비, 납부일, 납부총액

--- src/components/Completion/Middle.tsx
+++ src/components/Completion/Middle.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import useCustomSelector from '../../hooks/useCustomSelector';
+import { Info, InfoBox, Menu, MiddleWrapper } from '../../styles/Completion.style';
 
 const CompletionBoxMiddle = () => {
-  return <div>CompletionBoxMiddle</div>;
+  const { RentalType, MontlyRent, Deposit, MontlyFee, PaymentDate } = useCustomSelector().inputState;
+
+  return (
+    <MiddleWrapper>
+      <InfoBox>
+        <Menu>임대유형</Menu>
+        <Info>{RentalType}</Info>
+      </InfoBox>
+      <InfoBox>
+        <Menu>월임대료 </Menu>
+        <Info>
+          {MontlyRent}
+          {MontlyRent && '만원'}
+        </Info>
+      </InfoBox>
+      <InfoBox>
+        <Menu>보증금 </Menu>
+        <Info>{Deposit.toLocaleString()} 만원</Info>
+      </InfoBox>
+      <InfoBox>
+        <Menu>월관리비 </Menu>
+        <Info>{MontlyFee} 만원</Info>
+      </InfoBox>
+      <InfoBox>
+        <Menu>납부일 </Menu>
+        <Info>매달 {PaymentDate} 일 </Info>
+      </InfoBox>
+      <InfoBox>
+        <Menu>납부총액 </Menu>
+        <Info>{Number(MontlyRent) + Number(MontlyFee)} 만원</Info>
+      </InfoBox>
+    </MiddleWrapper>
+  );
 };
 
 export default CompletionBoxMiddle;
