1c1585029a7786d06df39335ba25a8d35855e109
ParkJeongHo <pjh31208@naver.com>
2023-02-28 20:48:11 +0900

    Feat(redux): Create reducer, action, store for managing data

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,20 @@ import App from './app';
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store/store';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
