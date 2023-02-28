d3f65e66334fa1a899d7a52feff55cc09772ab6a
ParkJeongHo <pjh31208@naver.com>
2023-02-28 20:31:21 +0900

    Feat(pages): Create pages folder

---
+++ src/pages/completionPage/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Completion from '../../components/Completion';
+
+const CompletetionPage = () => {
+  return <Completion />;
+};
+
+export default CompletetionPage;
