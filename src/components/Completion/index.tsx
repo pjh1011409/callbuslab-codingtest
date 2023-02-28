f546d3dcff6796613eebeb7bc99fc798924078e3
ParkJeongHo <pjh31208@naver.com>
2023-02-28 21:25:18 +0900

    Feat: Create root components for completionpage's components

--- src/components/Completion/index.tsx
+++ src/components/Completion/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-
+import { CompletionWrapper } from '../../styles/Completion.style';
 import CompletionBoxTop from './Top';
 import CompletionBoxMiddle from './Middle';
 import CompletionBoxBottom from './Bottom';
 
 const Completion = () => {
   return (
-    <>
+    <CompletionWrapper>
       <CompletionBoxTop />
       <CompletionBoxMiddle />
       <CompletionBoxBottom />
-    </>
+    </CompletionWrapper>
   );
 };
 
